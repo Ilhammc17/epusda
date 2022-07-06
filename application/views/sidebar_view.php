cdaf5f859fdb9000e17ae793bb89862b44ee3b62
Muhamad Ilham Ramdhani <rizalsangrilla77@gmail.com>
2022-07-06 18:50:09 +0700

    commit

--- application/views/sidebar_view.php
+++ application/views/sidebar_view.php
@@ -123,6 +123,11 @@
 					<i class="fa fa-money"></i> <span>Denda</span>
 					
 				</a>
+				<li class="<?php if($sidebar == 'pengadaan'){ echo 'active';}?>">
+				<a href="<?php echo base_url("sf");?>" class="cursor">
+					<i class="fa fa-book"></i> <span>Pengadaan</span>
+					
+				</a>
 			</li>
 			<li class="<?php if($sidebar == 'atur'){ echo 'active';}?>">
 				<a href="<?php echo base_url('dashboard/atur');?>">
